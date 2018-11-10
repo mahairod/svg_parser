835e2885ff4a9a7e2d267ea7eabe995ddc582776
Антон Астафьев <anton@astafiev.me>
2018-11-10 15:20:36 +0700

    испр. порядка в агрегатах

--- src/sql/affix-links.sql
+++ src/sql/affix-links.sql
@@ -1,9 +1,8 @@
 with
   aa as (
-	select count(*) qty, word, array_agg(id) aa, array_agg(affix) a,
-		array(select unnest(array_agg(offs)) v order by v) starts,
-		array(select unnest(array_agg(len)) v order by v) lens,
-		array(select unnest(array_agg(offs+len)) v order by v) ends
+	select count(*) qty, word,
+		array_agg(id order by offs) aa, array_agg(affix order by offs) a,
+		array_agg(offs order by offs) starts, array_agg(offs+len order by offs) ends
 	from affix_appl aa group by word
 )
 --	insert into composed_affix_appl
@@ -12,22 +11,6 @@ select word, aa[1], aa[2], aa[3], a[1], a[2], a[3],
 		ARRAY(select numrange(starts[i], ends[i]) from generate_subscripts(starts,1) g(i)) aff_locs
 from aa
 ;
-
-with
-  aa as (
-	select count(*) qty, word, array_agg(id) aa, array_agg(affix) a,
-		array(select unnest(array_agg(offs)) v order by v) starts,
-		array(select unnest(array_agg(len)) v order by v) lens,
-		array(select unnest(array_agg(offs+len)) v order by v) ends
-	from affix_appl aa group by word
-)
---	insert into composed_affix_appl
-select word, aa[1], aa[2], aa[3], a[1], a[2], a[3],
-		ARRAY(select numrange((1::smallint||ends)[i], starts[i]) from generate_series(1, 1+array_length(starts,1)) g(i)) val_locs,
-		ARRAY(select numrange(starts[i], ends[i]) from generate_subscripts(starts,1) g(i)) aff_locs
-from aa
-;
-
 
 select count(*) qty, word, array_agg(aa.id) aa, array_agg(aa.affix) a, a.kind, count (a.kind) kqty
 from affix_appl aa
